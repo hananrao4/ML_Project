587cb5021e0f22de6a5e230917a2d28ec9ad67f9
unknown <hananakram396@.com>
2023-12-24 17:09:38 +0300

    Model trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     logging.info('The execution has been started')
@@ -15,6 +16,9 @@ if __name__=="__main__":
 
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e:
         logging.info("Custom Exception")
